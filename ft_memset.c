56e8d979c9ab5ed886a9daa881c46dffb739419c
guizindev <guilhermevasco2015@gmail.com>
2024-09-25 19:17:41 -0300

    att

--- ft_memset.c
+++ ft_memset.c
@@ -1,16 +1,28 @@
 #include "libft.h"
-
-void *ft_memset(void *s, int c, size_t n)
+void	*ft_memset(void *s, int c, size_t n)
 {
-  size_t i;
-  unsigned  char *num_return;
-  
-  i = 0;
-  num_return = s;
-  while (i < n)
-  {
-    num_return[i] = (unsigned char)c;
-    i++;
-  }
-  return s;
-} 
+	size_t	i;
+	unsigned char *d;
+
+	d = (unsigned char *)s;
+	i = 0;
+	while (i < n)
+	{
+		*d = (unsigned char)c;
+		d++;
+		i++;
+	}
+	return (s);
+}
+int main()
+{
+	int arr[10];
+	//memset(arr,2,sizeof(int) *10);
+	ft_memset(arr,2,sizeof(int) * 10);
+	int i = 0;
+	while (i < 10)
+	{
+		printf("%d- %d\n",i ,arr[i]);
+		i++;
+	}
+}
